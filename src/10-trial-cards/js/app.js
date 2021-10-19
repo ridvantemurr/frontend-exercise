97892ecf101dc5be1b79325627743d5f4e2e306c
ridvantemurr <ridvan_temur10@hotmail.com>
2021-10-19 13:43:44 +0300

    tasarımın js kısmındaki error düzeltildi

--- src/10-trial-cards/js/app.js
+++
@@ -1,8 +0,0 @@
-function circleBar(elemId , percent){
-    let elem = document.getElementById(elemId);
-   let circles = elem.querySelectorAll('circle');
-   let r = circles[0].getAttribute('r');
-   console.log(r);
-   let circum = 2 * Math.PI * r;
-}
-circleBar('elem1', 50 );
